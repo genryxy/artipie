cf7c3488ee5547173e5163a5480a025373fa529c
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2020-12-29 09:50:14 +0300

    #813 - Change order

--- examples/gem/run.sh
+++ examples/gem/run.sh
@@ -12,20 +12,20 @@ ruby --version
 #sudo gem update --system
 gem --version
 # Push a gem into artipie.
+cd sample-project
+gem build sample-project.gemspec
+#cd ../../src/test/resources/gem
+#gem --version
 export GEM_HOST_API_KEY=$(echo -n "hello:world" | base64)
-#cd sample-project
-#gem build sample-project.gemspec
-cd ../../src/test/resources/gem
-gem --version
-#gem push sample-project-1.0.0.gem --host http://localhost:8080/my-gem
-gem push rails-6.0.2.2.gem --host http://localhost:8080/my-gem
+gem push sample-project-1.0.0.gem --host http://localhost:8080/my-gem
+#gem push rails-6.0.2.2.gem --host http://localhost:8080/my-gem
 
 cd ..
 
 gem --version
 # Fetch the uploaded earlier gem from artipie.
-#gem fetch sample-project --source http://localhost:8080/my-gem
-gem fetch rails --source http://localhost:8080/my-gem
+gem fetch sample-project --source http://localhost:8080/my-gem
+#gem fetch rails --source http://localhost:8080/my-gem
 
 
 # Remove container.
