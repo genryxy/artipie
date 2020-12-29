88005ddacd2cb07295dfb0e5abbf457b99e99311
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2020-12-29 09:09:28 +0300

    Specified another version, remove gem update

--- examples/gem/run.sh
+++ examples/gem/run.sh
@@ -9,7 +9,7 @@ sleep 5
 
 gem --version
 ruby --version
-sudo gem update --system
+#sudo gem update --system
 gem --version
 # Push a gem into artipie.
 export GEM_HOST_API_KEY=$(echo -n "hello:world" | base64)
