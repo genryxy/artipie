d0cd1ffb5c02b331c75fa4c1fd481edbc4f21e02
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2020-12-29 16:31:38 +0300

    #813 - Use only version

--- examples/gem/run.sh
+++ examples/gem/run.sh
@@ -9,8 +9,8 @@ sleep 5
 
 gem --version
 ruby --version
-sudo gem update --system
-sudo gem install rubygems-update --default
+# sudo gem update --system
+# sudo gem install rubygems-update --default
 sudo gem update --system '3.2.3'
 gem list
 gem --version
@@ -20,12 +20,13 @@ gem build sample-project.gemspec
 #cd ../../src/test/resources/gem
 #gem --version
 export GEM_HOST_API_KEY=$(echo -n "hello:world" | base64)
+ls
+sleep 10
 gem push sample-project-1.0.0.gem --host http://localhost:8080/my-gem
 #gem push rails-6.0.2.2.gem --host http://localhost:8080/my-gem
 
 cd ..
 
-gem --version
 # Fetch the uploaded earlier gem from artipie.
 gem fetch sample-project --source http://localhost:8080/my-gem
 #gem fetch rails --source http://localhost:8080/my-gem
