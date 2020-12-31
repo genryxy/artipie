695f8887bb0df9df4e9c22f6f1211bec77f3c19f
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2020-12-31 12:20:26 +0300

    #813 - Use /bin/bash everywhere, call with debug options

--- examples/gem/run.sh
+++ examples/gem/run.sh
@@ -11,14 +11,14 @@ sleep 5
 cd sample-project
 gem build sample-project.gemspec
 
-bash -c GEM_HOST_API_KEY=$(echo -n "hello:world" | base64) gem push sample-project-1.0.0.gem --host http://localhost:8080/my-gem
+/bin/bash -c GEM_HOST_API_KEY=$(echo -n "hello:world" | base64) gem push sample-project-1.0.0.gem --host http://localhost:8080/my-gem --debug --verbose --backtrace
 
 cd ..
 
 # Fetch the uploaded earlier gem from artipie.
-/bin/bash -c GEM_HOST_API_KEY=$(echo -n "hello:world" | base64) gem fetch sample-project --source http://localhost:8080/my-gem
+/bin/bash -c GEM_HOST_API_KEY=$(echo -n "hello:world" | base64) gem fetch sample-project --source http://localhost:8080/my-gem --debug --verbose --backtrace
 
-/bin/bash -c GEM_HOST_API_KEY=$(echo -n "hello:world" | base64) gem fetch sample-project --source http://localhost:8080/my-gemempty
+/bin/bash -c GEM_HOST_API_KEY=$(echo -n "hello:world" | base64) gem fetch sample-project --source http://localhost:8080/my-gemempty --debug --verbose --backtrace
 
 # Remove container.
 docker stop artipie
